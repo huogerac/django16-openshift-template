ec5dfc7063afff17a200795272f8cda838a214e2
Roger Camargo <roger.camargo@djenie.com>
2015-01-14 09:45:23 -0200

    Improving the project structure: wsgi.py

--- wsgi/myproject/myproject/wsgi.py
+++ wsgi/myproject/myproject/wsgi.py
@@ -7,19 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
-import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-
-if os.environ.has_key('OPENSHIFT_REPO_DIR'):
-    sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'myproject'))
-    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-    os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
-    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-    try:
-        execfile(virtualenv, dict(__file__=virtualenv))
-    except IOError:
-        pass
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
