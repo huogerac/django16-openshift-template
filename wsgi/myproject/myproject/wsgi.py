db4e671c85c757290d45eba78ef9867db2629842
Roger Camargo <roger.camargo@djenie.com>
2015-01-14 02:11:47 -0200

    Improving the project structure

--- wsgi/myproject/myproject/wsgi.py
+++ wsgi/myproject/myproject/wsgi.py
@@ -1,26 +1,20 @@
-"""
-WSGI config for openshift project.
+#!/usr/bin/env python
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
 
-if os.environ.has_key('OPENSHIFT_REPO_DIR'):
-    sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'myproject', 'myproject'))
-    #sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'myproject', 'myproject'))
-    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-    os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
-    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-    try:
-        execfile(virtualenv, dict(__file__=virtualenv))
-    except IOError:
-        pass
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
